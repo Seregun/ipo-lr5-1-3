8bbde9d54469752481fba944d8b55c46cf593000
Seregun <seregunmgp@gmail.com>
2024-10-20 20:52:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-with open('text.txt', 'r') as file: # Открытие файла text.txt для чтения
+with open('E:\\Python\\lr5_3\\text.txt', 'r') as file: # Открытие файла text.txt (указать путь к файлу) для чтения
     text = file.read() # Считывание текста из файла    
 words_count = len(text.split()) # Разделение текста на слова с помощью пробелов и подсчет их количества
 print(f"В файле text.txt {words_count} слов.") # Вывод результата
